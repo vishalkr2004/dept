8209d3963e5e8e773aff99d747ff7b0296631b61
Vishal KR <115525134+vishalkr2004@users.noreply.github.com>
2023-07-25 17:03:17 +0530

    Add files via upload

--- dep.js
+++ dep.js
@@ -38,3 +38,91 @@ function aero() {
       x.style.display = "none";
     }
   }
+  function civ() {
+    var x = document.getElementById("civ");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function cs() {
+    var x = document.getElementById("cs");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ee() {
+    var x = document.getElementById("ee");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function ed() {
+    var x = document.getElementById("ed");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function hs() {
+    var x = document.getElementById("hs");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function man() {
+    var x = document.getElementById("ms");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function math() {
+    var x = document.getElementById("math");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function me() {
+    var x = document.getElementById("me");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function mm() {
+    var x = document.getElementById("mm");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function oe() {
+    var x = document.getElementById("oe");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function phy() {
+    var x = document.getElementById("phy");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
