d74420d26104528891f4675895458d7fd0d24e48
Vishal KR <vishal22.raji@gmail.com>
2023-07-25 11:18:59 +0530

    first commit

---
+++ dep.js
@@ -0,0 +1,40 @@
+function aero() {
+    var x = document.getElementById("aero");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function am() {
+    var x = document.getElementById("am");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function bio() {
+    var x = document.getElementById("bio");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function chem() {
+    var x = document.getElementById("chem");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  function chy() {
+    var x = document.getElementById("chy");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
